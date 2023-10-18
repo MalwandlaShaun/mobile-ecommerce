1a52f9e68a7b482dca2df113077db0080b9a24bd
mochochokoboiketlo <mochochokoboiketlo@gmail.com>
2023-10-18 15:21:47 +0200

    my progress

--- ecommerce-mobile/Cart.js
+++ ecommerce-mobile/Cart.js
@@ -1,8 +1,62 @@
-import React from 'react';
-import { View, Text, SafeAreaView, StyleSheet } from 'react-native';
-import Icon from 'react-native-vector-icons/FontAwesome5';
+import React from "react";
+import { View, Text, SafeAreaView, StyleSheet, FlatList } from "react-native";
+import Icon from "react-native-vector-icons/FontAwesome5";
 
 const Cart = () => {
+  const cartItems = [
+    {
+      name: "Nike Air Max 200",
+      price: "R 240.00",
+      id: "1",
+    },
+    {
+      name: "Adidas Ultra Boost",
+      price: "R 320.00",
+      id: "2",
+    },
+    {
+      name: "Puma RS-X",
+      price: "R 180.00",
+      id: "3",
+    },
+    {
+      name: "Reebok Classic",
+      price: "R 200.00",
+      id: "4",
+    },
+    {
+      name: "New Balance 574",
+      price: "R 260.00",
+      id: "5",
+    },
+  ];
+
+  const renderItem = ({ item }) => (
+    <View style={styles.cart}>
+      <View style={styles.picture}></View>
+      <View style={styles.text}>
+        <View>
+          <Text style={styles.Discriptiontext}>{item.name}</Text>
+        </View>
+        <View>
+          <Text style={styles.Pricetext}>{item.price}</Text>
+        </View>
+      </View>
+
+      <View style={styles.IncreseDecrease}>
+        <View style={styles.Decrease}>
+          <Icon name="minus" size={10} color="black" solid />
+        </View>
+        <View style={styles.Number}>
+          <Text> 1 </Text>
+        </View>
+        <View style={styles.Increase}>
+          <Icon name="plus" size={10} color="black" solid />
+        </View>
+      </View>
+    </View>
+  );
+
   return (
     <SafeAreaView style={styles.container}>
       <View style={styles.secondContainer}>
@@ -15,118 +69,162 @@ const Cart = () => {
         <View style={styles.trashIcon}>
           <Icon name="trash" size={20} color="grey" solid />
         </View>
-        {/* Add your cart items here */}
       </View>
       <View style={styles.cartContainer}>
-        <View style = {styles.cart}>
-        <View style={styles.picture}>
-        
-        </View>
-        <View style={styles.text}>
-        <View>
-        <Text style={styles.Discriptiontext}>Nike Air Max 200</Text>
-        </View>
-        <View>
-        <Text style={styles.Pricetext}>R 200.00</Text>
-        </View>
-        </View>
+        <FlatList
+          data={cartItems}
+          renderItem={renderItem}
+          keyExtractor={(item) => item.id}
+          showsVerticalScrollIndicator={false}
+        />
+        <View style={styles.bottomCart}>
+          <View style={styles.TotalPirceAndTaxes}>
+            <View style={styles.subtotal}>
+              <Text style={styles.Pricetext}>Subtotal: R 1250</Text>
+            </View>
+            <View style={styles.taxes}>
+              <Text style={styles.Pricetext}>Taxes: R 40</Text>
+            </View>
+          </View>
+          <View style={styles.PirceAndCheckout}>
+             <View style={styles.TolatPrice}>
 
-        <View style={styles.IncreseDecrease}>
+             </View>
+             <View style={styles.checkOut}>
 
+             </View>
+             
+          </View>
         </View>
-       
-        <View style={styles.PirceAndText}>
-
-         </View>
-         <View style={styles.checkoutPirce}>
-
-         </View>
-        </View>
-       
-        {/* Add your cart items here */}
       </View>
     </SafeAreaView>
   );
 };
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'white',
+    backgroundColor: "white",
+    width: "100%",
   },
   arrowIcon: {
-    backgroundColor: 'white',
+    backgroundColor: "white",
   },
   textContainer: {
     flex: 1,
-    alignItems: 'center',
+    alignItems: "center",
   },
-  cartText:{
-   fontSize:24,
-   fontWeight:'bold',  
+  cartText: {
+    fontSize: 24,
+    fontWeight: "bold",
   },
   trashIcon: {
-    backgroundColor: 'white',
+    backgroundColor: "white",
   },
   secondContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    backgroundColor: 'white',
-    height: 60,
+    flexDirection: "row",
+    alignItems: "center",
+    backgroundColor: "white",
+    height: 100,
     paddingHorizontal: 10,
   },
   cartContainer: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#FDF5E6',
+    width: "100%",
+    backgroundColor: "#FDF5E6",
     height: 60,
-    // minWidth: 360,
-    paddingHorizontal: 20,
-    borderTopRightRadius:30,
-    borderTopLeftRadius:30
+    borderTopRightRadius: 30,
+    borderTopLeftRadius: 30,
+    justifyContent: "space-between",
+    alignItems: "center",
+    paddingTop: 20,
+  },
 
-  },
-  cart:{
-    flexDirection: 'row',
-    alignItems: 'center',
-    backgroundColor: 'white',
+  cart: {
+    flexDirection: "row",
+    alignItems: "center",
+    backgroundColor: "white",
     height: 105,
-    width:320,
+    width: 320,
     paddingHorizontal: 12,
-    borderRadius:32
+    borderRadius: 30,
+    marginBottom: 10, // Add margin bottom to create a gap between the cart components
+    justifyContent: "space-between",
   },
-  picture:{
-  backgroundColor: 'red',
-  height: 80,
-  minWidth: 80,
-  borderRadius: 40
-  },
-  text:{
+  picture: {
+    backgroundColor: "red",
     height: 80,
-    paddingLeft:20,
+    minWidth: 80,
+    borderRadius: 40,
+  },
+  text: {
+    height: 80,
+    paddingLeft: 20,
   },
   Discriptiontext: {
-    paddingTop:15,
+    paddingTop: 15,
     fontSize: 12,
-    fontWeight: 'bold', 
-    paddingBottom:10,  
+    fontWeight: "bold",
+    paddingBottom: 10,
   },
-  Pricetext:{
+  Pricetext: {
     fontSize: 16,
-    fontWeight: 'bold',
+    fontWeight: "bold",
   },
-  IncreseDecrease:{
+  IncreseDecrease: {
+    flex: 1,
+    alignItems: "center",
+    backgroundColor: "white",
+  },
+  TotalPirceAndTaxes: {
+    height: 50,
+    width: "100%",
+    backgroundColor: "#FDF5E6",
+    flexDirection:'row',
+    justifyContent: "space-between",
+    paddingHorizontal: 70,
+    alignItems: "center",
+  },
+  bottomCart: {
+    backgroundColor: "#FDF5E6",
+    width: "100%",
+  },
+  PirceAndCheckout: {
+    height: 105,
+    backgroundColor: "brown",
+    flexDirection: "row",
+    borderTopRightRadius: 30,
+    borderTopLeftRadius: 30,
     
   },
-  PirceAndText:{
-    height: 80,
-    backgroundColor:'red',
+  Decrease: {
+    backgroundColor: "white",
+    height: 16,
+    width: 16,
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 8,
+    fontWeight: "bold",
+    shadowColor: "black",
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.4,
+    shadowRadius: 2,
+    elevation: 4,
+  },
+
+  Increase: {
+    backgroundColor: "#90ffff",
+    height: 16,
+    width: 16,
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 8,
+    fontWeight: "bold",
+  },
+  subtotal:{
 
   },
-  checkoutPirce:{
-    height: 80,
-    backgroundColor:'red',
+  taxes:{
+
   }
 });
 
