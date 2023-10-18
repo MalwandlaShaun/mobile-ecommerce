51f506f465b5e9acf239effd0f1385c610cdd44e
mochochokoboiketlo <mochochokoboiketlo@gmail.com>
2023-10-18 15:45:14 +0200

    ristar again

--- ecommerce-mobile/App.jsx
+++ ecommerce-mobile/App.jsx
@@ -2,11 +2,14 @@ import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import LandingScreen from "./SRC/Screens/LandingScreen/Index";
 import DetailsScrean from "./SRC/Screens/DetailsScreen/index";
+import Cart from "./SRC/Screens/CartScreen";
 
 export default function App() {
   return (
     <>
-      <LandingScreen />
+       
+      {/* <LandingScreen /> */}
+      <Cart/>
     </>
   );
 }
