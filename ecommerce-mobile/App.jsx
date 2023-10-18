7993b7c716152620b1a0d828bd5c58bd54c8b852
Letlhogonolo01 <letlhogonolo.moroke8@gmail.com>
2023-10-18 16:02:28 +0200

    tlhogi - commit details

--- ecommerce-mobile/App.jsx
+++ ecommerce-mobile/App.jsx
@@ -1,20 +1,10 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
-import LandingScreen from "./SRC/Screens/LandingScreen/Index";
 import DetailsScrean from "./SRC/Screens/DetailsScreen/index";
 
 export default function App() {
   return (
     <>
-      <LandingScreen />
+      <DetailsScrean />
     </>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#F5F5F5",
-    padding: 20,
-  },
-});
